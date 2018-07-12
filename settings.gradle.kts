1db2ebf1484457cc40728f2faf13bdc6997f6034
StefMa <StefMaDev@outlook.com>
2018-07-12 15:04:04 +0200

    Remove android from settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,9 +8,6 @@ pluginManagement {
             if (requested.id.id == "guru.stefma.bintrayrelease") {
                 useModule("guru.stefma.bintrayrelease:bintrayrelease:${requested.version}")
             }
-            if(requested.id.id.startsWith("com.android.")) {
-                useModule("com.android.tools.build:gradle:$requested.id.version")
-            }
         }
     }
 }
