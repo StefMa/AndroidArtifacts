d941debd242d1937d7508b587e0f2e9a73a01424
StefMa <StefMaDev@outlook.com>
2018-07-16 15:52:54 +0200

    Extract method for publication creation

--- src/main/kotlin/guru/stefma/androidartifacts/JavaArtifactsPlugin.kt
+++ src/main/kotlin/guru/stefma/androidartifacts/JavaArtifactsPlugin.kt
@@ -12,9 +12,21 @@ class JavaArtifactsPlugin : Plugin<Project> {
         val publicationTasks = project.tasks.createListAvailablePublicationTask()
         val publicationName = "maven"
         project.tasks.createJavaArtifactsTask(publicationName)
-        val publicationContainer = project.publishingExtension.publications
         publicationTasks.publicationNames += publicationName
-        publicationContainer.create(publicationName, MavenPublication::class.java) {
+        createPublication(extension, project, publicationName, publicationTasks)
+    }
+
+    private fun Project.createJavaArtifactsExtension() =
+            extensions.create("javaArtifact", ArtifactsExtension::class.java)
+
+    private fun createPublication(
+            extension: ArtifactsExtension,
+            project: Project,
+            publicationName: String,
+            publicationTasks: ListGeneratedPublicationTasks
+    ) {
+        publicationTasks.publicationNames += publicationName
+        project.publishingExtension.publications.create(publicationName, MavenPublication::class.java) {
             it.from(project.components.getByName("java"))
             // Publish sources only if set to true
             if (extension.sources) it.addJavaSourcesArtifact(project, "maven")
@@ -26,9 +38,5 @@ class JavaArtifactsPlugin : Plugin<Project> {
             it.setupMetadata(project, extension)
         }
     }
-
-
-    private fun Project.createJavaArtifactsExtension() =
-            extensions.create("javaArtifact", ArtifactsExtension::class.java)
 
 }
