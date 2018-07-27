30e5373a5e93a2799e91aa152722ba7617170626
StefMa <StefMaDev@outlook.com>
2018-07-27 14:29:37 +0200

    Increase version to 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ gradlePlugin {
 }
 
 group = "guru.stefma.androidartifacts"
-version = "1.0.0"
+version = "1.1.0"
 configure<PublishExtension> {
     artifactId = "androidartifacts"
     userOrg = "stefma"
