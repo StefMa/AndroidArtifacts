52e0f4bb1cebf9c70ebe27b98f9c143ad5fb69bb
StefMa <StefMaDev@outlook.com>
2018-09-20 10:43:10 +0200

    Add valid java consumer

--- subprojects/consumer/java/src/main/java/guru/stefma/androidartifacts/java/Example.java
+++ subprojects/consumer/java/src/main/java/guru/stefma/androidartifacts/java/Example.java
@@ -1,14 +1,16 @@
 package guru.stefma.androidartifacts.java;
 
+import org.mockito.Mockito;
+
 /**
  * This <b>Exmaple</b> class will only be used to
  * show how to use the <i>guru.stefma.javaartifacts</i> Gradle plugin.
  */
-class Example {
+public class Example {
 
     /**
      * This static method will print the given <b>string</b> parameter
-     * to the {@link System.out}
+     * to the {@link System#out}
      *
      * @param string the string which will be displayed
      */
@@ -21,6 +23,17 @@ class Example {
      */
     protected void canBeOverriden() {
 
+    }
+
+    /**
+     * An example that shows that classes
+     * from dependencies (like the {@link org.junit.runners.JUnit4}) are
+     * inside the javadoc.
+     *
+     * @return a valid {@link Mockito} instance.
+     */
+    public Mockito ignoreMe() {
+        return new Mockito();
     }
 
 }
