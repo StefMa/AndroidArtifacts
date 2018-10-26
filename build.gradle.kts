bfabcf8f785cf7a43422d9ac6043c437793bbe4d
StefMa <StefMaDev@outlook.com>
2018-10-26 16:54:05 +0200

    Use runtimeOnly for jupiter engine

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.3.1")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
     testImplementation("org.assertj:assertj-core:3.11.1")
 }
 
