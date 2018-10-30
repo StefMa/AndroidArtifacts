9d0148e98bf1408e3ee856f509a6fc3ca269d781
StefMa <StefMaDev@outlook.com>
2018-10-30 11:13:00 +0100

    Make tasks lazy

--- buildSrc/src/main/kotlin/guru/stefma/buildsrc/ZeitPlugin.kt
+++ buildSrc/src/main/kotlin/guru/stefma/buildsrc/ZeitPlugin.kt
@@ -2,21 +2,22 @@ package guru.stefma.buildsrc
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskContainer
 
 class ZeitPlugin : Plugin<Project> {
 
     override fun apply(project: Project): Unit = with(project) {
-        tasks.create("moveDocsToNow", MoveDokkaAndGradleSiteToNow::class.java) {
-            it.dependsOn("dokka", "generateSite")
+        tasks.register("moveDocsToNow", MoveDokkaAndGradleSiteToNow::class.java) {
+            it.dependsOn(tasksNamed("dokka", "generateSite"))
         }
-        tasks.create("createNowDockerfile", CreateNowDockerfile::class.java)
-        tasks.create("createNowEntrypoint", CreateNowEntrypointIndexHtml::class.java)
-        tasks.create("createNowJson", CreateNowJson::class.java)
+        tasks.register("createNowDockerfile", CreateNowDockerfile::class.java)
+        tasks.register("createNowEntrypoint", CreateNowEntrypointIndexHtml::class.java)
+        tasks.register("createNowJson", CreateNowJson::class.java)
 
         // This task requires a valid now-cli installation...
         // Alternatively you can put a now token via gradle properties in.
-        tasks.create("publishDocsToNow") {
-            it.dependsOn("moveDocsToNow", "createNowDockerfile", "createNowEntrypoint", "createNowJson")
+        tasks.register("publishDocsToNow") {
+            it.dependsOn(tasksNamed("moveDocsToNow", "createNowDockerfile", "createNowEntrypoint", "createNowJson"))
 
             it.doLast {
                 project.exec {
@@ -32,8 +33,8 @@ class ZeitPlugin : Plugin<Project> {
             }
         }
 
-        tasks.create("createNowAlias") {
-            it.dependsOn("publishDocsToNow")
+        tasks.register("createNowAlias") {
+            it.dependsOn(tasksNamed("publishDocsToNow"))
 
             it.doLast {
                 project.exec {
@@ -51,3 +52,11 @@ class ZeitPlugin : Plugin<Project> {
     }
 
 }
+
+/**
+ * A shorthand over [TaskContainer.named].
+ *
+ * Will return an array for each given [taskNames].
+ */
+private fun Project.tasksNamed(vararg taskNames: String) =
+        taskNames.map { tasks.named(it) }.toTypedArray()
